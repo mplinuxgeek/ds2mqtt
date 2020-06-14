1ae21301a5bec26700bffeeaca21fa2d9e26f041
Martin <bircoe@gmail.com>
2020-06-14 16:09:06 +0100

    Update ds2mqtt.py

--- ds2mqtt.py
+++ ds2mqtt.py
@@ -37,18 +37,18 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload)) 
 
-def device_config(id, type):
+def device_config(id, name):
     device = {}
     device['unit_of_measurement'] = degree + 'C'
     device['icon'] = 'mdi:thermometer'
-    device['name'] = sensor.id
-    device['state_topic'] = topic + '/sensor/' + sensor.id + '/state'
-    device['unique_id'] = sensor.id
+    device['name'] = id
+    device['state_topic'] = topic + '/sensor/' + id + '/state'
+    device['unique_id'] = id
     device['device'] =  {
-        "identifiers": sensor.id,
+        "identifiers": id,
         "manufacturer": "Dallas",
-        "model": sensor_type_name,
-        "name": sensor.id,
+        "model": name,
+        "name": id,
     }
     return json.dumps(device)
 
@@ -63,27 +63,12 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 homeassistant = get_config_safe('general', 'homeassistant')
-interval = get_config_safe('general', 'interval')
+interval = get_config_safe('general', 'interval','30')
 broker = get_config_safe('mqtt', 'broker')
-port = get_config_safe('mqtt', 'port')
+port = get_config_safe('mqtt', 'port', '1884')
 user = get_config_safe('mqtt', 'user')
 password = get_config_safe('mqtt', 'pass')
 topic = get_config_safe('mqtt', 'topic')
-
-THERM_SENSOR_DS18S20 = 0x10
-THERM_SENSOR_DS1822 = 0x22
-THERM_SENSOR_DS18B20 = 0x28
-THERM_SENSOR_DS1825 = 0x3B
-THERM_SENSOR_DS28EA00 = 0x42
-THERM_SENSOR_MAX31850K = 0x3B
-TYPE_NAMES = {
-    THERM_SENSOR_DS18S20: "DS18S20",
-    THERM_SENSOR_DS1822: "DS1822",
-    THERM_SENSOR_DS18B20: "DS18B20",
-    THERM_SENSOR_DS1825: "DS1825",
-    THERM_SENSOR_DS28EA00: "DS28EA00",
-    THERM_SENSOR_MAX31850K: "MAX31850K",
-}
 
 degree = u"\N{DEGREE SIGN}"
 
@@ -102,17 +87,14 @@ while not client.connected:
 
 if homeassistant == "true":
     for sensor in W1ThermSensor.get_available_sensors():
+        device = device_config(sensor.id, sensor.type_name)
         print("Publishing config for sensor " + sensor.id)
-        sensor_type_name = TYPE_NAMES.get(sensor.type, hex(sensor.type))
-        device = device_config(sensor.id, sensor_type_name)
         client.publish("homeassistant/sensor/" + sensor.id + "/temp/config",device)
 
 while True:
     for sensor in W1ThermSensor.get_available_sensors():
-        sensor_type_name = TYPE_NAMES.get(sensor.type, hex(sensor.type))
         temperature = sensor.get_temperature()
-        sensor_id = sensor.id
-        print("%s %s %.2f" % (sensor_type_name, sensor_id, temperature))
-        client.publish(topic + "/sensor/" + sensor_id +'/state', round(temperature,2))
+        print("%s %s %.2f%sC" % (sensor.type_name, sensor.id, temperature, degree))
+        client.publish(topic + "/sensor/" + sensor.id +'/state', round(temperature,2))
     print("Sleeping for %s seconds" % (interval))
     time.sleep(float(interval))
