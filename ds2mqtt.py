fe33d012a7eb6c816595a9bd182ef0c20fc56e90
MartinP <bircoe@gmail.com>
2020-06-16 01:10:34 +0930

    Update ds2mqtt.py

--- ds2mqtt.py
+++ ds2mqtt.py
@@ -145,7 +145,7 @@ def publish_config(sensors):
 def publish_sensors(sensors):
     for sensor in sensors:
         temp = sensor.get_temperature()
-        logger.info("%s %s %.2f%sC" % (sensor.type_name, sensor.id, temp, degree))
+        logger.info("Publishing sensor %s %s %.2f%sC" % (sensor.type_name, sensor.id, temp, degree))
         if client.connected is True:
             client.publish(topic + "/sensor/" + sensor.id + '/state', round(temp, 2))
         else:
