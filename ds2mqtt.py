22832741d64fdf36aafc99af1108426eea9399f8
MartinP <bircoe@gmail.com>
2020-06-17 10:45:15 +0930

    Update ds2mqtt.py

--- ds2mqtt.py
+++ ds2mqtt.py
@@ -101,7 +101,9 @@ def connect_to_broker(host):
     # client.on_publish = on_publish
     if username != "" and password != "":
         client.username_pw_set(username, password=password)
-    client.connect(host)
+    try:
+        client.connect(host)
+    
     client.loop_start()
     while not client.connected:
         time.sleep(0.2)
